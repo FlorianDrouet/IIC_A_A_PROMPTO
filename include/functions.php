39099df146a7dded82ade10c063ba2e5ecccc545
Jourdain Stevens <stevens.jourdain@etudiants.univ-reims.fr>
2016-12-06 09:51:52 +0100

    Ajout d'une fonction de réduction de texte

--- include/functions.php
+++ include/functions.php
@@ -1,3 +1,17 @@
 <?php
 
+function reduireText($text)
+{
+	$mots = explode(' ', $text);
+	$nbMots = sizeof($mots);
+	if($nbMots > 30)
+		$nbMots = 30;
+
+	$newText = array();
+	for($i = 0; $i < $nbMots; $i++)	
+		$newText []= $mots[$i];
+	
+	return implode(' ', $newText).'...';
+}
+
 ?>
